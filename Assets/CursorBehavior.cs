f9bbafaf70dbfea0465c1ffc39b3d6008394eac0
Andrew Gardner <timebot@gmail.com>
2017-09-30 12:53:49 -0700

    Initial code push

---
+++ Assets/CursorBehavior.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorBehavior : MonoBehaviour {
+    private Vector2 pos;
+    private Vector3 size;
+    public GameObject map;
+
+	// Use this for initialization
+	void Start () {
+        pos = transform.position;
+        size = GetComponent<SpriteRenderer>().bounds.size;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = pos;
+
+        pos.x = Mathf.Floor(Input.mousePosition.x / 16) - map.transform.position.x;
+        pos.y = Mathf.Floor(Input.mousePosition.y / 16) - map.transform.position.y + (size.y / 2);
+
+        print(pos);
+    }
+}
