6ae94a426b226e52bdb0d535cf9dbdb92d185aa3
Andrew Gardner <timebot@gmail.com>
2018-01-07 00:44:44 -0800

    Add GameHandler class

--- Assets/Scripts/GameHandler.cs
+++ Assets/Scripts/GameHandler.cs
@@ -2,9 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class GameHandler : MonoBehaviour {
 	private SmartGridBehavior smartGrid;
 	private PlayerManager playerManager;
+
+	// For now, hard-coding unit values into code - eventually will extract from Tile files
 
 	// Use this for initialization
 	void Start () {
